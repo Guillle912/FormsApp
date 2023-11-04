599381b7d375e0700429f324d7ed1b954e3a9dc2
Guillermo Vidal <Guillle912@gmail.com>
2023-11-04 17:08:57 +0100

    Menu de navegacion realizado

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPgeComponent } from './pages/register-pge/register-pge.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'sign-up', component: RegisterPgeComponent},
+      { path: '**', redirectTo: 'sign-up'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
